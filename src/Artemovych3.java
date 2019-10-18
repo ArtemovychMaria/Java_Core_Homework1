0fe596bd8b2b01756601bcbd7eab91592bcad374
Artemovych Maria <mari4ka.art.bloody@gmail.com>
2019-10-18 10:21:02 +0300

    first commit

---
+++ src/Artemovych3.java
@@ -0,0 +1,29 @@
+public class Artemovych3 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        int c=6;
+    }
+
+    /**
+     *
+     * @param a перше число
+     * @param b друге число
+     * @param c третє число
+     * @return результат обчислень
+     */
+    public static int count1(int a,int b,int c){
+        return (a+b)/c;
+    }
+
+    /**
+     *
+     * @param a перше число
+     * @param b друге число
+     * @return результат обчислень
+     */
+    public static int count2(int a,int b){
+        return (a-b)+3;
+    }
+
+}
---
+++ src/Artemovych4.java
@@ -0,0 +1,13 @@
+//Завдання 2
+public class Artemovych4 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE + "  min: " + Integer.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE + "  min: " + Double.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE + "  min:" + Float.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE + "  min:" +Short.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE + "  min:" + Long.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE + "  min" + Byte.MIN_VALUE);
+        System.out.println((int)Character.MAX_VALUE + "  min:" + (int)Character.MIN_VALUE);
+    }
+
+}
